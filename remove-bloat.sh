c668e18efe09829cc6bc0b980034210b7b470531
johansarelvdberg <81371706+johansarelvdberg@users.noreply.github.com>
2023-01-08 15:23:22 +0200

    Update remove-bloat.sh

--- remove-bloat.sh
+++ remove-bloat.sh
@@ -3,6 +3,10 @@
 # Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre)
 sudo apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre -y
 
+# install new packges
+sudo apt install npm
+sudo curl -fsSL https://code-server.dev/install.sh | sh
+
 # Autoremove
 sudo apt-get autoremove -y
 
